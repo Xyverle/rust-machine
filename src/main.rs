88738e305193e5d54860d0b301b79dfd9b019737
maxscout <doctorrooskie1@gmail.com>
2022-06-19 21:53:18 -0400

    some cool window stuff

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,19 @@
 
 use bevy::prelude::*;
 
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+
 fn main() {
     println!("Hello, world!!");
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+    .insert_resource(ClearColor(CLEAR))
+    .insert_resource(WindowDescriptor {
+        width: 1600.0,
+        height: 900.0,
+        title: "Cool".to_string(),
+        resizable: true,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins)
+    .run();
 }
