1065f26a0a7d1686bd96b702587c2737f50e6481
maxscout <doctorrooskie1@gmail.com>
2022-06-20 19:37:29 -0400

    cool example thingy

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,54 @@
+#![allow(unused_must_use)]
 #![allow(non_snake_case)]
 #![allow(clippy::redundant_field_names)]
-
 use bevy::{prelude::*, render::camera::ScalingMode};
 
-pub const CLEAR: Color = Color::rgb(0.1, 0.2, 0.3);
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 pub const TILE_SIZE: f32 = 0.1;
 
+mod ascii;
+mod debug;
 mod player;
+mod tilemap;
+
+use ascii::AsciiPlugin;
+use debug::DebugPlugin;
 use player::PlayerPlugin;
+use tilemap::TileMapPlugin;
 
 fn main() {
-    println!("Hello, world!!");
+    let height = 900.0;
     App::new()
-    .insert_resource(ClearColor(CLEAR))
-    .insert_resource(WindowDescriptor {
-        width: 1600.0,
-        height: 900.0,
-        title: "Cool".to_string(),
-        resizable: true,
-        ..Default::default()
-    })
-    .add_startup_system(spawn_camera)
-    .add_plugin(PlayerPlugin)
-    .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
-    .add_plugins(DefaultPlugins)
-    .run();
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: height * RESOLUTION,
+            height: height,
+            title: "cool".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_camera)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(AsciiPlugin)
+        .add_plugin(DebugPlugin)
+        .add_plugin(TileMapPlugin)
+        .run();
 }
-
-
 
 fn spawn_camera(mut commands: Commands) {
     let mut camera = OrthographicCameraBundle::new_2d();
 
+    //Set the camera to have normalized coordinates of y values -1 to 1
     camera.orthographic_projection.top = 1.0;
     camera.orthographic_projection.bottom = -1.0;
 
     camera.orthographic_projection.right = 1.0 * RESOLUTION;
     camera.orthographic_projection.left = -1.0 * RESOLUTION;
-    
+
+    //Force the camera to use our settings
     camera.orthographic_projection.scaling_mode = ScalingMode::None;
 
     commands.spawn_bundle(camera);
-}
-
-struct AsciiSheet(Handle<TextureAtlas>);
-
-fn load_ascii(
-    mut commands: Commands,
-    assets: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let image = assets.load("Ascii.png");
-    let atlas =
-        TextureAtlas::from_grid_with_padding(image, Vec2::splat(9.0), 16, 16, Vec2::splat(2.0));
-
-    let atlas_handle = texture_atlases.add(atlas);
-
-    commands.insert_resource(AsciiSheet(atlas_handle));
 }
