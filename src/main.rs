d8e011345446dc9890e78f547048f3e77b0eeae8
maxscout <doctorrooskie1@gmail.com>
2022-06-20 16:10:25 -0400

    moving stuff

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #![allow(non_snake_case)]
+#![allow(clippy::redundant_field_names)]
 
 use bevy::{prelude::*, render::camera::ScalingMode};
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.2, 0.3);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
+
+mod player;
+use player::PlayerPlugin;
 
 fn main() {
     println!("Hello, world!!");
@@ -17,49 +22,13 @@ fn main() {
         ..Default::default()
     })
     .add_startup_system(spawn_camera)
-    .add_startup_system(spawn_player)
+    .add_plugin(PlayerPlugin)
     .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
     .add_plugins(DefaultPlugins)
     .run();
 }
 
-fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
-    let mut sprite = TextureAtlasSprite::new(1);
-    sprite.color = Color::rgb(0.3, 0.3, 0.9);
-    sprite.custom_size = Some(Vec2::splat(1.0));
 
-    let player = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: sprite,
-            texture_atlas: ascii.0.clone(),
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 900.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(Name::new("Player"))
-        .id();
-
-    let mut background_sprite = TextureAtlasSprite::new(0);
-    background_sprite.color = Color::rgb(0.5, 0.5, 0.5);
-    background_sprite.custom_size = Some(Vec2::splat(1.0));
-
-    let background = commands
-        .spawn_bundle(SpriteSheetBundle {
-            sprite: background_sprite,
-            texture_atlas: ascii.0.clone(),
-            transform: Transform {
-                translation: Vec3::new(0.0, 0.0, -1.0),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(Name::new("Background"))
-        .id(); //id() gives back the entity after creation
-
-    commands.entity(player).push_children(&[background]);
-}
 
 fn spawn_camera(mut commands: Commands) {
     let mut camera = OrthographicCameraBundle::new_2d();
